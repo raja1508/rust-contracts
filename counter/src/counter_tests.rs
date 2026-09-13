196944983313fe6d90b4657cd91c51e7eb1ce980
raza <rajar120805@gmail.com>
2026-09-13 21:12:02 +0530

    Contract 01 - Counter

---
+++ counter/src/counter_tests.rs
@@ -0,0 +1,7 @@
+use std::assert_eq;
+
+
+#[test]
+pub fn initialize_counter_test() {
+    assert_eq!(4 + 4, 8); 
+}
--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -1,14 +1,93 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshDeserialize, BorshSerialize, Clone)]
+pub struct Counter {
+    pub count : u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(BorshDeserialize, BorshSerialize)]
+pub enum InstructionData {
+    Initialize,
+    Increase(u32),
+    Decrease(u32)
+    
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
+// #[cfg(custom_heap)]
+entrypoint!(process_instructions); 
+
+pub fn process_instructions(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+
+    let mut iter = accounts.iter(); 
+    let counter_account = next_account_info(&mut iter)?; 
+
+    if !counter_account.is_signer {
+        return Err(ProgramError::MissingRequiredSignature); 
+    }
+
+    if counter_account.owner != program_id{
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    if instruction_data.is_empty() {
+        return Err(ProgramError::InvalidInstructionData); 
+    }
+
+    
+    let instruction = InstructionData::try_from_slice(&instruction_data)?; 
+
+    
+    let data = match instruction {
+        InstructionData::Initialize => {
+            let counter = Counter{
+                count: 0
+            }; 
+            counter
+        },
+        _ => {
+            let data = counter_account.data.borrow();
+            let mut counter : Counter = Counter::try_from_slice(&data)?; 
+            process_counter(&mut counter, instruction)? 
+        }
+    }; 
+
+    data.serialize(&mut *counter_account.data.borrow_mut())?;
+
+    
+
+    Ok(())
+}
+
+fn process_counter(counter: &mut Counter, instruction: InstructionData ) -> Result<Counter, ProgramError>{
+    match instruction {
+        InstructionData::Increase(value) => {
+            let x = counter.count.checked_add(value).unwrap(); 
+            counter.count = x;
+            return Ok(counter.clone()) 
+        },
+        InstructionData::Decrease(value) => {
+            let x = counter.count.checked_sub(value).unwrap(); 
+            counter.count = x; 
+            return Ok(counter.clone())
+        },
+        _ => {
+            return Err(ProgramError::InvalidInstructionData)
+        }
     }
 }
+
+
+#[cfg(test)]
+mod counter_tests;
